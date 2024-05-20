4a6c787758cf4c0d3e3a95de5351ea380a759c5d
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-05-20 12:03:06 +0300

    Update Input.jsx

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Input = () => {
-  return <div>Input</div>;
+const Input = ({ taskList, setTaskList }) => {
+  const [input, setInput] = useState("");
+
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    setTaskList([...taskList, input]);
+    setInput("");
+  };
+
+  return (
+    <>
+      <form>
+        <input
+          className="border rounded px-2"
+          type="text"
+          placeholder="Add a task"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={handleAddTask}>Add</button>
+      </form>
+    </>
+  );
 };
 
 export default Input;
