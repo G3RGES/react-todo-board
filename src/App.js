0a19f222c28bb4be3fcdabedfa2059db9aa7532c
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-05-20 12:10:19 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
   // console.log(taskList);//*TESTING
 
   return (
-    <>
-      <h1>TODO BOARD</h1>
+    <div className="flex flex-col items-center justify-center py-8 gap-4">
+      <h1 className="font-semibold text-xl">TODO BOARD</h1>
       <Input taskList={taskList} setTaskList={setTaskList} />
       <div className="">
         {taskList.map((task, idx) => (
           <li key={idx}>{task}</li>
         ))}
       </div>
-    </>
+    </div>
   );
 }
 
