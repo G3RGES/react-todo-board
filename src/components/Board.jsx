a2e3104d0dde7063e08067c87e46f9916e46a130
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-05-20 12:30:33 +0300

    Update Board.jsx

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
-const Board = () => {
-  return <div>Board</div>;
+const Board = ({ task, index, taskList, setTaskList }) => {
+  const handleDelete = () => {
+    let removeIndex = taskList.indexOf(task);
+    taskList.splice(removeIndex, 1);
+    setTaskList((currentTasks) =>
+      currentTasks.filter((task) => index === removeIndex)
+    );
+  };
+
+  return (
+    <>
+      <div
+        className="max-w-md flex flex-col sm:flex-row gap-3 items-center justify-start border
+      rounded-lg text-center text-lg pt-3 pb-4 px-4 md:px-6"
+      >
+        <p>{task}</p>
+        <button
+          className="bg-red-600 rounded-lg px-2.5 py-1 text-white font-semibold
+           hover:opacity-70"
+          onClick={handleDelete}
+        >
+          Delete
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Board;
