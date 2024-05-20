36dde3f000c928ab628bae58c63fa8944bcf6c61
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-05-20 12:02:35 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+import Input from "./components/Input";
+
 function App() {
+  const [taskList, setTaskList] = useState([]);
+
+  // console.log(taskList);//*TESTING
+
   return (
     <>
       <h1>TODO BOARD</h1>
+      <Input taskList={taskList} setTaskList={setTaskList} />
+      <div className="">
+        {taskList.map((task, idx) => (
+          <li key={idx}>{task}</li>
+        ))}
+      </div>
     </>
   );
 }
